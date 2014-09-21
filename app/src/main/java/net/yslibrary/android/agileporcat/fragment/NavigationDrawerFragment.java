8fe131cf805dd2ae1e448584b2b76ca2dc71f74b
yshrsmz <the.phantom.bane@gmail.com>
2014-09-21 20:38:02 +0900

    modify package structure & add otto, butterknife

--- app/src/main/java/net/yslibrary/android/agileporcat/fragment/NavigationDrawerFragment.java
+++ app/src/main/java/net/yslibrary/android/agileporcat/fragment/NavigationDrawerFragment.java
@@ -1,4 +1,4 @@
-package net.yslibrary.android.agileporcat;
+package net.yslibrary.android.agileporcat.fragment;
 
 
 import android.app.Activity;
@@ -21,6 +21,8 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import net.yslibrary.android.agileporcat.R;
 
 /**
  * Fragment used for managing interactions for and presentation of a navigation drawer.
